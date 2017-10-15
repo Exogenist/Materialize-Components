9576facae563a9bebcd99090f6e0439999a70b89
Swainson Holness <swainson.holness@gmail.com>
2017-10-14 18:51:54 -0600

    Update main.js

--- navigation/sidebar/main.js
+++ navigation/sidebar/main.js
@@ -4,19 +4,41 @@ window.onload = function(){
     var hamburger = document.querySelector(".ham-box");
     var logo = document.querySelector(".sideNavLogo h1");
     var links = document.querySelector(".sidenav a");
-    var background = document.querySelector("body");
+    var background = document.querySelector(".overlay");
+    var token = false
 
     closeBtn.addEventListener("click", function(event){
         event.preventDefault();
+        background.style.cursor = "default";
+        token = false;
         sideNav.style.width = "0px";
-//        sideNav.style.overflow = "hidden";
-//        sideNav.style.whiteSpace = "nowrap";
-        logo.style.opacity = "0";       
+        logo.style.opacity = "0";
+        background.style.backgroundColor = "rgba(0, 0, 0, 0.0)";   
     });
     
     hamburger.addEventListener("click", function(event){
         event.preventDefault();
+        background.style.cursor = "pointer";
         sideNav.style.width = "280px";
         logo.style.opacity = "1";
+        background.style.zIndex = "1";
+        background.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+        token = true;
+    });
+
+    background.addEventListener("click", function(event){
+        event.preventDefault();
+        token = false;
+        sideNav.style.width = "0px";
+        logo.style.opacity = "0";
+        background.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+        background.style.cursor = "default";
+           
+    });
+
+    background.addEventListener("transitionend", function(event){
+        if(token === false) {
+            background.style.zIndex = "-1";
+        }   
     });
 }
